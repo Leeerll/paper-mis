151e8797e81d7f525e70140f3aa0a5d8556d0268
Yuebing233 <104044941+Yuebing233@users.noreply.github.com>
2022-06-09 17:43:00 +0800

    2.0

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,91 +1,114 @@
 <template>
   <el-container style="min-height: 100vh">
-    <el-aside :width="sideWidth + 'px'" style="background-color: rgb(238, 241, 246); box-shadow: 2px 0 6px rgb(0 21 41 / 35%);">
+
+    <el-aside :width="sideWidth + 'px'" style="box-shadow: 2px 0 6px rgb(0 21 41 );">
       <el-menu :default-openeds="['1', '3']" style="min-height: 100%; overflow-x: hidden"
-               background-color="rgb(42,43,45)"
+               background-color="rgb(48, 65, 86)"
                text-color="#fff"
-               active-text-color="rgb(110,179,255)"
+               active-text-color="#ffd04b"
                :collapse-transition="false"
                :collapse="isCollapse"
       >
         <div style="height: 60px; line-height: 60px; text-align: center">
-          <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; margin-right: 5px">
-          <b style="color: white" v-show="logoTextShow">论文管理系统</b>
+          <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; right: 5px">
+          <b style="color: white" v-show="logoTextShow">后台管理系统</b>
         </div>
         <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-message"></i>
-            <span slot="title">查找论文</span>   <!--//导航1-->
+            <span slot="title">导航一</span>
           </template>
-          <el-menu-item-group>
-            <template slot="title">根据作者查找</template>  <!--//分组1-->
-            <el-menu-item index="1-1">学生</el-menu-item>    <!--//选项1-->
-            <el-menu-item index="1-2">老师</el-menu-item>    <!--//选项2-->
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
           </el-menu-item-group>
-          <el-menu-item-group title="根据论文查找">   <!--//分组2-->
-            <el-menu-item index="1-3">论文编号</el-menu-item>   <!--//选项3-->
-            <el-menu-item index="1-4">论文名称</el-menu-item>   <!--//选项4-->
-            <el-menu-item index="1-5">论文类别</el-menu-item>   <!--//选项5-->
-            <el-menu-item index="1-6">论文等级</el-menu-item>   <!--//选项6-->
-          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+          </el-submenu>
         </el-submenu>
         <el-submenu index="2">
           <template slot="title">
             <i class="el-icon-menu"></i>
-            <span slot="title">对论文的操作</span>    <!--//导航2-->
+            <span slot="title">导航二</span>
           </template>
-          <el-menu-item-group>
-            <el-menu-item index="2-1">上传论文</el-menu-item>   <!--//选项1-->
-            <el-menu-item index="2-2">删除论文</el-menu-item>   <!--//选项2-->
-            <el-menu-item index="2-3">修改论文</el-menu-item>   <!--//选项3-->
-          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+          </el-submenu>
         </el-submenu>
         <el-submenu index="3">
           <template slot="title">
             <i class="el-icon-setting"></i>
-            <span slot="title">个人中心</span>
+            <span slot="title">导航三</span>
           </template>
-<!--          <el-menu-item-group>-->
-<!--            <template slot="title">分组一</template>-->
-<!--            <el-menu-item index="3-1">选项1</el-menu-item>-->
-<!--            <el-menu-item index="3-2">选项2</el-menu-item>-->
-<!--          </el-menu-item-group>-->
-<!--          <el-menu-item-group title="分组2">-->
-<!--            <el-menu-item index="3-3">选项3</el-menu-item>-->
-<!--          </el-menu-item-group>-->
-<!--          <el-submenu index="3-4">-->
-<!--            <template slot="title">选项4</template>-->
-<!--            <el-menu-item index="3-4-1">选项4-1</el-menu-item>-->
-<!--          </el-submenu>-->
+          <el-submenu index="3-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+          </el-submenu>
         </el-submenu>
       </el-menu>
     </el-aside>
 
     <el-container>
-
       <el-header style="font-size: 12px; border-bottom: 1px solid #ccc; line-height: 60px; display: flex">
         <div style="flex: 1; font-size: 20px">
           <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
         </div>
         <el-dropdown style="width: 70px; cursor: pointer">
-          <span>Leeer</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>个人信息</el-dropdown-item>
-            <el-dropdown-item>退出</el-dropdown-item>
+          <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+          <el-dropdown-menu slot="dropdown" >
+            <el-dropdown-item style="font-size: 14px; padding: 5px 0">个人信息</el-dropdown-item>
+            <el-dropdown-item style="font-size: 14px; padding: 5px 0">退出</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
 
       </el-header>
 
       <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="number" label="论文编号" width="140">
+        <div style="margin-bottom: 30px">
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
+
+        <div style="margin: 10px 0">
+          <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search"></el-input>
+          <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5"></el-input>
+          <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5"></el-input>
+          <el-button class="ml-5" type="primary">搜索</el-button>
+        </div>
+
+        <div style="margin: 10px 0">
+          <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+          <el-button type="danger">批量删除 <i class="el-icon-remove-outline"></i></el-button>
+          <el-button type="primary">导入 <i class="el-icon-bottom"></i></el-button>
+          <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
+        </div>
+
+        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
+          <el-table-column prop="date" label="日期" width="140">
           </el-table-column>
-          <el-table-column prop="name" label="论文名称" width="320">
+          <el-table-column prop="name" label="姓名" width="120">
           </el-table-column>
-          <el-table-column prop="date" label="发表时间">
+          <el-table-column prop="address" label="地址">
+          </el-table-column>
+          <el-table-column label="操作"  width="200" align="center">
+            <template slot-scope="scope">
+              <el-button type="success">编辑 <i class="el-icon-edit"></i></el-button>
+              <el-button type="danger">删除 <i class="el-icon-remove-outline"></i></el-button>
+            </template>
           </el-table-column>
         </el-table>
+        <div style="padding: 10px 0">
+          <el-pagination
+
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="10"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="400">
+          </el-pagination>
+        </div>
       </el-main>
 
     </el-container>
@@ -98,16 +121,18 @@ export default {
   name: 'Home',
   data() {
     const item = {
-      number: '1001',
-      name: '基于情感分析和PCA-LSTM模型的股票价格预测',
-      date: '2022-05-27',
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
     };
     return {
-      tableData: Array(1).fill(item),
+      tableData: Array(10).fill(item),
+      msg: "hello 青哥哥",
       collapseBtnClass: 'el-icon-s-fold',
       isCollapse: false,
       sideWidth: 200,
-      logoTextShow: true
+      logoTextShow: true,
+      headerBg: 'headerBg'
     }
   },
   methods: {
@@ -126,3 +151,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.headerBg {
+  background: #eee!important;
+}
+</style>
